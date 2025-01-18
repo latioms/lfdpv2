d7dc5fbd94520cd792d79e372ab4164190c2c778
latioms <latioms@gmail.com>
2025-01-18 20:49:18 +0000

    feat(sidebar): suppression du composant NavProjects et nettoyage des imports dans app-sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -11,11 +11,9 @@ import {
   BarChart2,
   ClipboardList,
   FileText,
-  Settings,
 } from "lucide-react";
 
 import { NavMain } from "@/components/nav-main";
-import { NavProjects } from "@/components/nav-projects";
 import { NavUser } from "@/components/nav-user";
 import { TeamSwitcher } from "@/components/team-switcher";
 import {
@@ -25,8 +23,6 @@ import {
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar";
-import { useRouter } from "next/navigation";
-import { useAuthContext } from "./providers/AuthProvider";
 
 // This is sample data.
 const data = {
